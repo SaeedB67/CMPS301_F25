bfec6bf85acf577b432328338e0c8e733403c3a3
SaeedB67 <saeed.babun@laverne.edu>
2025-10-28 22:14:38 -0700

    Add files via upload
    Quiz 2

--- quiz.cpp
+++ quiz.cpp
@@ -1,61 +1,43 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+int main() {
 
-int main()
-{
-    cout << "============MY PRODUCTIVE DAY SIMULATION============" << endl;
-    cout << "Activity breakdown:" << endl;
+	int smallCoffeesOrdered;
+	cout << "Enter number of small coffees: ";
+	cin >> smallCoffeesOrdered;
 
-    int day = 1440;
-    int sleep = 132;
-    string classList = "Business Statistics, Rhetoric Studies, Programming Concepts, and Publishing on the Web";
-    string extracurricularList = "Job, Commuting, and Meals";
-    string funList = "Youtube, video games, working out, and coding";
+	int mediumCoffeesOrdered;
+	cout << "Enter number of medium coffees: ";
+	cin >> mediumCoffeesOrdered;
 
-    int homework = 120;
-    int meals = 60;
-    int workingOut = 100;
-    int commuting = 0;
-    int job = 120;
-    int coding = 60;
-    int youtube = 170;
-    int videoGames = 320;
-    int businessStatistics = 95;
-    int rhetoricStudies = 65;
-    int programmingConcepts = 95;
-    int publishingOnTheWeb = 95;
+	int largeCoffeesOrdered;
+	cout << "Enter number of large coffees: ";
+	cin >> largeCoffeesOrdered;
 
-    int totalProductiveTime = homework + job + coding + businessStatistics + rhetoricStudies + programmingConcepts + publishingOnTheWeb;
-    int totalLeisureTime = meals + workingOut + commuting + youtube + videoGames;
-    int totalClasses = businessStatistics + rhetoricStudies + programmingConcepts + publishingOnTheWeb;
-    int totalExtracurriculars = job + meals;
-    int totalFun = youtube + videoGames + workingOut + coding;
+	double taxRate;
+	cout << "Enter tax rate (%): ";
+	cin >> taxRate;
+	cout << "================= COFFEE ORDER RECEIPT =================" << endl;
 
-    int remainingTime = day - (totalProductiveTime + totalLeisureTime) - sleep;
+	double totalSmallCoffeesOrdered = smallCoffeesOrdered * 2.25;
+	cout << "Small Coffees Ordered (" << smallCoffeesOrdered << " x " << "$2.25): $" << totalSmallCoffeesOrdered << endl;
+	double totalMediumCoffeesOrdered = mediumCoffeesOrdered * 3.00;
+	cout << "Medium Coffees Ordered (" << mediumCoffeesOrdered << " x " << "$3.00): $" << totalMediumCoffeesOrdered << endl;
+	double totalLargeCoffeesOrdered = largeCoffeesOrdered * 3.75;
+	cout << "Large Coffees Ordered (" << largeCoffeesOrdered << " x " << "$3.75): $" << totalLargeCoffeesOrdered << endl;
 
-    int totalTime = totalLeisureTime + totalProductiveTime;
-    int hours = totalTime / 60;
-    int minutes = totalTime % 60;
+	double subtotal = totalSmallCoffeesOrdered + totalMediumCoffeesOrdered + totalLargeCoffeesOrdered;
+	double tax = subtotal * (taxRate / 100);
+	double total = subtotal + tax;
 
-    double percentageProductive = (static_cast<double>(totalProductiveTime) / totalTime) * 100.0;
-
-    cout << "Sleep: " << sleep / 60 << " hours and " << sleep % 60 << " minutes" << endl;
-    cout << "Classes: " << classList << endl;
-	cout << "Total Class Time: " << totalClasses / 60 << " hours and " << totalClasses % 60 << " minutes" << endl;
-    cout << "Homework: " << homework / 60 << " hours and " << homework % 60 << " minutes" << endl;
-	cout << "================================================" << endl;
-    cout << "Fun: " << funList << endl;
-    cout << "Extra Curriculars: " << extracurricularList<< " minutes" << endl;
-	cout << "Total Extracurricular Time and Fun Time: " << ( totalExtracurriculars + totalFun ) / 60 << " hours and " << (totalExtracurriculars + totalFun) % 60<<" minutes" << endl;
-    cout << "Commuting: " << commuting / 60 << " hours and " << commuting % 60 << " minutes" << endl;
-	cout << "================================================" << endl;
-    cout << "Total Time Spent: " << hours << " hours and " << minutes << " minutes" << endl;
-    cout << "Total Productive Time: " << totalProductiveTime /60 << " hours and " << totalProductiveTime % 60 <<" minutes" << endl;
-    cout << "Total Leisure Time: " << totalLeisureTime /60 << " hours and " << totalLeisureTime % 60 <<" minutes" << endl;
-    cout << "Remaining Time: " << remainingTime << " minutes" << endl;
-    cout << "You used " << percentageProductive << "% of your time productively." << endl;
-    cout << "Amazing job today! Can you do better tomorrow? :)" << endl;
-    cout << "=====================================================" << endl;
-
-    return 0;
+	cout << fixed << setprecision(2) << showpoint;
+	cout << "--------------------------------------------------------" << endl;
+	cout << left << setw(30) << setfill('.') << "Subtotal:" << "$" << subtotal << endl;
+	cout << "Tax (" << fixed << setprecision(2) << taxRate << "%): " << right << setw(10) << "$" << tax << endl;
+	cout << "--------------------------------------------------------" << endl;
+	cout << left << setw(30) << setfill('.') << "Total Amount Due:" << "$" << total << endl;
+	cout << "========================================================" << endl;
+	cout << "Thank you for supporting our local coffee shop!" << endl;
+	return 0;
 }
